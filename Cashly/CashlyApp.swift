855905351874b6a7c5d131f8669aeb266427e17a
giovannimhern <giovannimhern@uchicago.edu>
2024-03-14 19:09:26 -0500

    Initial Commit

---
+++ Cashly/CashlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashlyApp.swift
+//  Cashly
+//
+//  Created by Giovanni Maya on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct CashlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
